453a3ebf8db90d4919ade1657e93a04a46d3703d
Louis Knolle <knollelw@gmail.com>
2022-07-23 12:00:13 -0400

    fix nav tabs styling

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,31 @@ homeTab.onclick = function() {
   projectsSection.setAttribute("class", "hidden");
   homeImage.setAttribute("class", "photos");
   homeImage.classList.remove("hidden");
+  aboutTab.classList.remove("current-item");
+  projectsTab.classList.remove("current-item");
+  projectsTab.setAttribute("class", "bar-item");
+  aboutTab.setAttribute("class", "bar-item");
 }
 
 aboutTab.onclick = function() {
   homeImage.setAttribute("class", "hidden");
   projectsSection.setAttribute("class", "hidden")
+  aboutTab.setAttribute("class", "current-item")
   aboutSection.classList.remove("hidden");
+  homeTab.classList.remove("current-item");
+  projectsTab.classList.remove("current-item");
+  homeTab.setAttribute("class", "bar-item");
+  projectsTab.setAttribute("class", "bar-item");
 }
 
 projectsTab.onclick = function() {
   aboutSection.setAttribute("class", "hidden");
   homeImage.setAttribute("class", "hidden")
+  projectsTab.setAttribute("class", "current-item")
   projectsSection.classList.remove("hidden");
+  homeTab.classList.remove("current-item");
+  aboutTab.classList.remove("current-item");
+  homeTab.setAttribute("class", "bar-item");
+  aboutTab.setAttribute("class", "bar-item");
 }
 
