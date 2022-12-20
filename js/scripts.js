601221e4d815a3ca5b76b020190c33d88bd5a3d8
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-12-20 19:19:53 +0000

    embed youtube vid of trailpanion

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,41 @@
-const homeTab = document.getElementById("homeIcon");
-const aboutTab = document.getElementById("aboutIcon");
-const projectsTab = document.getElementById("projectsIcon");
+// const homeTab = document.getElementById("homeIcon");
+// const aboutTab = document.getElementById("aboutIcon");
+// const projectsTab = document.getElementById("projectsIcon");
 
-const homeImage = document.getElementById("img-container");
-const aboutSection = document.getElementById("about");
-const projectsSection = document.getElementById("projects");
+// const homeImage = document.getElementById("img-container");
+// const aboutSection = document.getElementById("about");
+// const projectsSection = document.getElementById("projects");
 
-homeTab.onclick = function() {
-  aboutSection.setAttribute("class", "hidden");
-  projectsSection.setAttribute("class", "hidden");
-  homeImage.setAttribute("class", "photos");
-  homeImage.classList.remove("hidden");
-  aboutTab.classList.remove("current-item");
-  projectsTab.classList.remove("current-item");
-  homeTab.setAttribute("class", "current-item")
-  projectsTab.setAttribute("class", "bar-item");
-  aboutTab.setAttribute("class", "bar-item");
-}
+// homeTab.onclick = function() {
+//   aboutSection.setAttribute("class", "hidden");
+//   projectsSection.setAttribute("class", "hidden");
+//   homeImage.setAttribute("class", "photos");
+//   homeImage.classList.remove("hidden");
+//   aboutTab.classList.remove("current-item");
+//   projectsTab.classList.remove("current-item");
+//   homeTab.setAttribute("class", "current-item")
+//   projectsTab.setAttribute("class", "bar-item");
+//   aboutTab.setAttribute("class", "bar-item");
+// }
 
-aboutTab.onclick = function() {
-  homeImage.setAttribute("class", "hidden");
-  projectsSection.setAttribute("class", "hidden")
-  aboutTab.setAttribute("class", "current-item")
-  aboutSection.classList.remove("hidden");
-  homeTab.classList.remove("current-item");
-  projectsTab.classList.remove("current-item");
-  homeTab.setAttribute("class", "bar-item");
-  projectsTab.setAttribute("class", "bar-item");
-}
+// aboutTab.onclick = function() {
+//   homeImage.setAttribute("class", "hidden");
+//   projectsSection.setAttribute("class", "hidden")
+//   aboutTab.setAttribute("class", "current-item")
+//   aboutSection.classList.remove("hidden");
+//   homeTab.classList.remove("current-item");
+//   projectsTab.classList.remove("current-item");
+//   homeTab.setAttribute("class", "bar-item");
+//   projectsTab.setAttribute("class", "bar-item");
+// }
 
-projectsTab.onclick = function() {
-  aboutSection.setAttribute("class", "hidden");
-  homeImage.setAttribute("class", "hidden")
-  projectsTab.setAttribute("class", "current-item")
-  projectsSection.classList.remove("hidden");
-  homeTab.classList.remove("current-item");
-  aboutTab.classList.remove("current-item");
-  homeTab.setAttribute("class", "bar-item");
-  aboutTab.setAttribute("class", "bar-item");
-}
-
+// projectsTab.onclick = function() {
+//   aboutSection.setAttribute("class", "hidden");
+//   homeImage.setAttribute("class", "hidden")
+//   projectsTab.setAttribute("class", "current-item")
+//   projectsSection.classList.remove("hidden");
+//   homeTab.classList.remove("current-item");
+//   aboutTab.classList.remove("current-item");
+//   homeTab.setAttribute("class", "bar-item");
+//   aboutTab.setAttribute("class", "bar-item");
+// }
